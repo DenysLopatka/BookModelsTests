eab0d827f603320d40ef90eee14b80d19cc969d8
DenysLopatka <qa.denys.lopatka@gmail.com>
2021-04-30 15:21:24 +0300

    2 tests registration

--- 2904Project/2904Project/UnitTest1.cs
+++ 2904Project/2904Project/UnitTest1.cs
@@ -22,11 +22,11 @@ namespace _2904Project
             _webDriver.Manage().Timeouts().PageLoad = TimeSpan.FromSeconds(60);
         }
 
-        [TearDown]
-        public void TearDown()
-        {
-            _webDriver.Quit();
-        }
+        //[TearDown]
+        //public void TearDown()
+        //{
+        //    _webDriver.Quit();
+        //}
 
         [Test]
         public void FirstRegistrationPage()
@@ -66,7 +66,55 @@ namespace _2904Project
         [Test]
         public void SecondRegistrationPage()
         {
+            DateTime dataTime = DateTime.Now;
+            var mailAdd = dataTime.Year.ToString() + dataTime.Month.ToString() + dataTime.Day.ToString() + dataTime.Hour.ToString() + dataTime.Millisecond.ToString();
 
+            _webDriver.Navigate().GoToUrl("https://newbookmodels.com/join");
+
+            var nameField = _webDriver.FindElement(By.CssSelector("[name = 'first_name']"));
+            nameField.SendKeys("Test");
+
+            var lastNameField = _webDriver.FindElement(By.CssSelector("[name = 'last_name']"));
+            lastNameField.SendKeys("Test");
+
+            var emailField = _webDriver.FindElement(By.CssSelector("[name = 'email']"));
+            emailField.SendKeys($"{mailAdd}b@laraskey.com");
+
+            var password = _webDriver.FindElement(By.CssSelector("[name = 'password']"));
+            password.SendKeys("Test123#");
+
+            var confirmPassword = _webDriver.FindElement(By.CssSelector("[name = 'password_confirm']"));
+            confirmPassword.SendKeys("Test123#");
+
+            var phoneNumber = _webDriver.FindElement(By.CssSelector("[name = 'phone_number']"));
+            phoneNumber.SendKeys("123.123.1221");
+
+            _webDriver.FindElement(By.CssSelector("[type= 'submit']")).Click();
+
+            System.Threading.Thread.Sleep(3000);                      
+
+            var companyName = _webDriver.FindElement(By.CssSelector("[name = 'company_name']"));
+            companyName.SendKeys("Test");
+
+            var companySite = _webDriver.FindElement(By.CssSelector("[name = 'company_website']"));
+            companySite.SendKeys("Test.com");
+
+            var location = _webDriver.FindElement(By.CssSelector("[name = 'location']"));
+            location.SendKeys("b");
+            _webDriver.FindElement(By.CssSelector("[class = 'pac-item-query']")).Click();
+
+            _webDriver.FindElement(By.CssSelector("[name = 'industry']")).Click();
+            System.Threading.Thread.Sleep(1500);
+            _webDriver.FindElements(By.CssSelector("[role = 'option']"))[2].Click();
+
+            System.Threading.Thread.Sleep(1000);
+            _webDriver.FindElement(By.CssSelector("[type = 'submit']")).Click();        
+
+            System.Threading.Thread.Sleep(3000);
+
+            var url = _webDriver.Url;
+
+            Assert.AreEqual("https://newbookmodels.com/explore", url);
         }
     }
 }
